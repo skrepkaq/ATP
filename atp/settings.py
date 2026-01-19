d382f6040068e1f70cc2caaef510a2317e6370ca
skrepkaq <skrepkaq@gmail.com>
2026-01-20 00:08:32 +0800

    other: settings refactor

--- atp/settings.py
+++ atp/settings.py
@@ -23,37 +23,37 @@ if DOCKER and docker_settings_file.exists():
     load_dotenv(docker_settings_file, override=True)
 
 
+# Настройки импорта видео
+IMPORT_LIKED_VIDEOS: bool = os.getenv("IMPORT_LIKED_VIDEOS", "true").lower() == "true"
+IMPORT_FAVORITE_VIDEOS: bool = os.getenv("IMPORT_FAVORITE_VIDEOS", "true").lower() == "true"
+
+# Настройки тиктока
+DOWNLOAD_FROM_TIKTOK: bool = os.getenv("DOWNLOAD_FROM_TIKTOK", "true").lower() == "true"
+TIKTOK_USER: str = os.getenv("TIKTOK_USER", "")
+
+# Настройки Telegram
+TELEGRAM_BOT_TOKEN: str = os.getenv("TELEGRAM_BOT_TOKEN", "")
+TELEGRAM_CHAT_ID: str = os.getenv("TELEGRAM_CHAT_ID", "")
+
+# Настройки проверки доступности
+CHECK_INTERVAL_DAYS: int = int(os.getenv("CHECK_INTERVAL_DAYS", "7"))
+
+# Пытаться скачать failed видео, вдруг их восстановили
+HOPE_MODE: bool = os.getenv("HOPE_MODE", "false").lower() == "true"
+
+# Количество попыток при скачивании/проверке видео
+MAX_RETRIES: int = int(os.getenv("MAX_RETRIES", "3"))
+
+# Настройки папок и базы
 DATABASE_FILE = os.getenv("DATABASE", "tiktok_videos.db")
 if not os.path.isabs(DATABASE_FILE):
     DATABASE_FILE = str(config_dir / DATABASE_FILE)
 DATABASE_URL: str = f"sqlite:///{DATABASE_FILE}"
 
 DOWNLOADS_DIR: str = os.getenv("DOWNLOADS_DIR", "/downloads")
+
 TIKTOK_DATA_FILE: str = os.getenv("TIKTOK_DATA_FILE", "user_data_tiktok.json")
 if not os.path.isabs(TIKTOK_DATA_FILE):
     TIKTOK_DATA_FILE = str(config_dir / TIKTOK_DATA_FILE)
 
-# Настройки Telegram
-TELEGRAM_BOT_TOKEN: str = os.getenv("TELEGRAM_BOT_TOKEN", "")
-TELEGRAM_CHAT_ID: str = os.getenv("TELEGRAM_CHAT_ID", "")
-
-# Настройки тиктока
-DOWNLOAD_FROM_TIKTOK: bool = os.getenv("DOWNLOAD_FROM_TIKTOK", "true").lower() == "true"
-TIKTOK_USER: str = os.getenv("TIKTOK_USER", "")
-
-# Настройки проверки доступности
-CHECK_INTERVAL_DAYS: int = int(os.getenv("CHECK_INTERVAL_DAYS", "7"))
-
-
-# Настройки импорта видео
-IMPORT_LIKED_VIDEOS: bool = os.getenv("IMPORT_LIKED_VIDEOS", "true").lower() == "true"
-IMPORT_FAVORITE_VIDEOS: bool = os.getenv("IMPORT_FAVORITE_VIDEOS", "true").lower() == "true"
-
-# Пытаться скачать failed видео, вдруг их восстановили
-HOPE_MODE: bool = os.getenv("HOPE_MODE", "false").lower() == "true"
-
-# Настройки временных директорий
 TMP_DIR: Path = Path(tempfile.gettempdir()) / "gallery_dl"
-
-# Настройки для retry логики
-MAX_RETRIES: int = int(os.getenv("MAX_RETRIES", "3"))
